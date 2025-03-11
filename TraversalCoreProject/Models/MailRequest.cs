2ed94c30f6fb6d9f429dd18933c2f1b31abd144c
Hazal Savran <hazalsavran@gmail.com>
2025-03-11 15:54:40 +0300

    Send mail process

---
+++ TraversalCoreProject/Models/MailRequest.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.Intrinsics.X86;
+
+namespace TraversalCoreProject.Models
+{
+    public class MailRequest
+    {
+        public string Name { get; set; }
+        public string Sender { get; set; }
+        public string Receiver { get; set; }
+        public string Subject { get; set; }
+        public string Body { get; set; }
+
+    }
+}
