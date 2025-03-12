fc637e8317d9b1a4cce8fcb68b52d0b8f67ec8b2
Hazal Savran <hazalsavran@gmail.com>
2025-03-12 19:47:33 +0300

    City process

---
+++ TraversalCoreProject/Models/CityClass.cs
@@ -0,0 +1,13 @@
+﻿
+
+using MailKit.Net.Pop3;
+
+namespace TraversalCoreProject.Models
+{
+    public class CityClass
+    {
+        public int CityId { get; set; }
+        public string CityName { get; set; }
+        public string CityCountry { get; set; }
+    }
+}
