f9fa7d4971485099bca830956e7be4645724e53f
Hazal Savran <hazalsavran@gmail.com>
2025-04-17 19:30:48 +0300

    added signalR api consume

---
+++ SignalRApi/DAL/Visitor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SignalRApi.DAL
+{
+    public enum ECity {         
+        NewYork = 1,
+        LosAngeles =2,
+        Chicago = 3,
+        Houston = 4,
+        Phoenix = 5
+    }
+    public class Visitor
+    {
+        public int VisitorID { get; set; }
+        public ECity City { get; set; }
+        public int CityVisitCount { get; set; }
+        public DateTime VisitDate { get; set; }
+    }
+}
